5fa4078a22d9e9ed016b97b1ad1ba4888ad6d9af
Hector Zarate <hector.zarate@daikinapplied.com>
2025-06-28 11:39:42 -0600

    svt

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,13 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
-  return (
+
+    return(
+      <Image source={require('@/assets/images/saveTheDate.png')} />
+      
+    );  
+/*return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
@@ -52,7 +52,8 @@ export default function HomeScreen() {
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
-  );
+  );*/
+
 }
 
 const styles = StyleSheet.create({
