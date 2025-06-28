3613054ab1993054bf809eeeca71a8b6605a282a
Hector Zarate <hector.zarate@daikinapplied.com>
2025-06-28 12:45:01 -0600

    prueba

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
     <meta property="og:title" content="Alex y Erick ♡ 30 de Mayo del 2026" />
     <meta property="og:description" content="Save the date! 30 - 5 - 202" />
-    <meta property="og:image" content={require('@/assets/images/saveTheDate.png')} />
+    <meta property="og:image" content={require('@/assets/images/saveTheDate_mini.png')} />
     <meta property="og:url" content="https://alexyerick.com" />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
