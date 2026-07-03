7dddb5454883f23bccab126fe138381fa8bd45e7
Hector Zarate <hector.zarate@daikinapplied.com>
2026-07-02 19:42:17 -0600

    saveTheDate

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,15 @@
 import { useLocalSearchParams } from "expo-router";
 import React from "react";
-import InvitacionScreen from "./(tabs)/invitacion";
+//import InvitacionScreen from "./(tabs)/invitacion";
 import SaveTheDateScreen from "./(tabs)/savethedate";
 
 export default function HomeScreen() {
   const pageType = process.env.EXPO_PUBLIC_PAGE_TYPE || 'savethedate';
   const params = useLocalSearchParams();
   console.log("environment variable EXPO_INDEX_PAGE:", process.env.EXPO_PUBLIC_PAGE_TYPE);
-  if (pageType === 'invitacion') {
-    return <InvitacionScreen />;
-  }
+  // if (pageType === 'invitacion'&& false) {
+  //   return <InvitacionScreen />;
+  // }
   
   return <SaveTheDateScreen />;
 }
