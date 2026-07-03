58f97cebc6cc97ae1bdbeef7e78081eb8a32574a
Hector Zarate <hector.zarate@daikinapplied.com>
2026-07-02 19:26:17 -0600

    saveTheDate

--- app/(tabs)/savethedate.tsx
+++ app/(tabs)/savethedate.tsx
@@ -6,23 +6,9 @@ export default function HomeScreen() {
 
   return (
     <View style={styles.container}>
-      <iframe 
-        height="75%" 
-        src="https://www.youtube.com/embed/velNa1Or318?autoplay=1&mute=1&loop=1&playlist=velNa1Or318&controls=1&modestbranding=1&rel=0" 
-        title="Acompañanos a escribir este nuevo capítulo."
-        allow="autoplay; encrypted-media; web-share"
-        allowFullScreen
-        frameBorder="0"
-        style={{
-          border: 'none',
-          borderRadius: '10px',
-          boxShadow: '0 4px 20px rgba(0,0,0,0.1)',
-          aspectRatio: '9/16',
-        }}
-      />
       <Image 
         style={styles.bannerImage}
-        source={require('@/assets/images/banner_saveTheDate.png')} 
+        source={require('@/assets/images/saveTheDate.jpeg')} 
       />
     </View>
   );
@@ -31,14 +17,13 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: '3%',
+    paddingTop: '0%',
     alignItems: 'center',
-    backgroundColor: '#000',
+    backgroundColor: '#999',
   },
   bannerImage: {
-    height: '20%',
-    width: '90%',
+    height: '100%',
     resizeMode: 'contain',
-    marginTop: '3%',
+    marginTop: '0%',
   },
 });
